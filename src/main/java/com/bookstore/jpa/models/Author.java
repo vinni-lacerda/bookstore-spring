f602de1b429ac614d4647a64309c28b8665add55
Vinicius Lacerda <vini.lacerda@hotmail.com>
2025-10-27 14:07:18 -0300

    feat: author model implemented

---
+++ src/main/java/com/bookstore/jpa/models/Author.java
@@ -0,0 +1,44 @@
+package com.bookstore.jpa.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_author")
+public class Author implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany
+    private Set<Book> books = new HashSet<>();
+
+    public Author(Long id, String name, Set<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+}
